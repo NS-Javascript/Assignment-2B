9b64238d1fb3a1392a3a2a308c0e2c2970c3206c
Aaron Furtado <aaron.furtado@wwindia.com>
2016-10-24 12:33:24 +0530

    Updated Validation process

---
+++ static/js/validation.js
@@ -0,0 +1,76 @@
+(function() {
+  'use strict';
+
+  $(document).ready(function (event) {
+
+    var validationOnIDs = [
+      "firstname",
+      "lastname",
+      "phone",
+      "office",
+      "email",
+      "password",
+      "password",
+      "month",
+      "day",
+      "year",
+      "age",
+      "residence1",
+      "residence2",
+      "checkbox_sample18",
+      "checkbox_sample19",
+      "checkbox_sample20",
+      "gender",
+      "nextStep"
+    ];
+
+
+    var validateFirstName = function (eventData) {
+
+    };
+
+    var validateLastName = function (eventData) {
+
+    };
+
+
+    var handleBlurEvent = function (eventSource, eventData) {
+      switch (eventSource) {
+        case "firstname":
+          validateFirstName(eventData);
+          break;
+        case "lastname":
+          validateLastName(eventData);
+          break;
+        default:
+
+      }
+    };
+
+    var handleClickEvent = function (eventSource, eventData) {
+
+    };
+
+    var hadleEvent = function (eventType, eventSource, eventData) {
+      switch (eventType) {
+        case "blur":
+        handleBlurEvent(eventSource, eventData);
+          break;
+        case "click":
+        handleClickEvent(eventSource, eventData);
+          break;
+        default:
+
+      }
+
+    };
+
+    for (var i = 0; i < validationOnIDs.length; i++) {
+      $("#" + validationOnIDs[i]).on("blur click", function (event) {
+        var eventType = event.type;
+        var eventSource = event.delegateTarget.id;
+        hadleEvent(eventType, eventSource, event);
+      });
+    }
+  });
+}());
