962e6d22be80308e7c97555ab3c2169bc2855d82
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-10-24 20:38:03 +0530

    firstname lastname final empty blur

--- static/js/validation.js
+++ static/js/validation.js
@@ -24,34 +24,80 @@
       "nextStep"
     ];
 
-/*validation First Name*/
+
+/*validation First Name------------------------------------------------------ */
 var validateFirstName = function (eventData) {
-//console.log(eventData);
-var eventTag=eventData.delegateTarget;
-var length=eventTag.value.length;
-//console.log(length);
-if (length==0) {
-  $(eventData.delegateTarget).after('<span class="error-message" id="ediv1" style="display : table-row-group">plz valid</span>');
-  return false;
-  }else {
-  $("#ediv1").remove();
-  return true;
-  }
+    var eventTag=eventData.delegateTarget;
+    var length=eventTag.value.length;
+        if(length==0) {
+          if(eventData.currentTarget.parentElement.childElementCount==2)
+              $(eventData.delegateTarget).after
+              ('<span class="error-message" id="ediv1" style="display : table-row-group">Please Fill First Name</span>');
+              return false;
+                      }
+          else {
+              $("#ediv1").remove();
+              return true;
+            }
 }
 
-/*validation Last Name*/
-    var validateLastName = function (eventData) {
+
+/*validation Last Name-------------------------------------------------------- */
+var validateLastName = function (eventData) {
       var eventTag=eventData.delegateTarget;
       var length=eventTag.value.length;
-      //console.log(length);
-      if (length==0) {
-        $(eventData.delegateTarget).after('<span class="error-message" id="ediv2" style="display : table-row-group">plz valid</span>');
-        return false;
-        }else {
+          if(length==0) {
+            if(eventData.currentTarget.parentElement.childElementCount==2)
+            $(eventData.delegateTarget).after
+            ('<span class="error-message" id="ediv2" style="display : table-row-group">Please Fill Last Name</span>');
+              return false;
+            }
+        else {
         $("#ediv2").remove();
         return true;
         }
-      }
+}
+
+
+/*validation Phone ------------------------------------------------------------*/
+  /*Phone -Empty--------------------------------------------------- */
+    var validatePhone = function (eventData) {
+    var eventTag=eventData.delegateTarget;
+    console.log(eventTag.value);
+    var length=eventTag.value.length;
+        if(length==0) {
+        if(eventData.currentTarget.parentElement.childElementCount==2)
+        $(eventData.delegateTarget).after
+        ('<span class="error-message" id="ediv3" style="display : table-row-group">Please Fill Phone Name</span>');
+          return false;
+        }
+    else {
+    $("#ediv3").remove();
+    return true;
+    }
+  /*Phone -10 digit Validation--------------------------------------------------- */
+      var patt = /^([0-9]){10,10}$/;
+      console.log(eventTag.value);
+        if(true){
+            if(eventData.currentTarget.parentElement.childElementCount==2)
+              $(eventData.delegateTarget).after
+                ('<span class="error-message" id="ediv3" style="display : table-row-group">Please Fill 10 Digits Phone Name</span>');
+                return false;
+            }
+          else {
+          $("#ediv3").remove();
+          return true;
+          }
+        }
+
+if (true) {
+
+} else if (true) {
+
+} {
+
+}
+
 
 
 
@@ -63,6 +109,9 @@ if (length==0) {
           break;
         case "lastname":
           validateLastName(eventData);
+          break;
+          case "phone" :
+          validatePhone(eventData);
           break;
         default:
 
