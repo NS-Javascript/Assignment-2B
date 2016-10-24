8e35efe2dd1a1ebcd6c85bfd90da95a5bec0caa7
rahulsapkal23 <rahul.sapkal23@neosofttech.com>
2016-10-24 20:20:34 +0530

    office + phone no+ pass validation

--- static/js/validation.js
+++ static/js/validation.js
@@ -23,8 +23,7 @@
             "gender",
             "nextStep"
         ];
-        var flag = 0;
-        var flag1 = 0;
+
 
         var validateFirstName = function(eventData) {
             //  console.log(eventData);
@@ -32,14 +31,14 @@
             if (eventData.delegateTarget.value == "" || eventData.delegateTarget.value == null) {
                 if ($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount == 0) {
                     $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s1' style='color:red'>Please Fill the First name</span>");
-                    //  flag = 1;
+
                     console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
                 }
 
             } else {
                 {
                     $("#s1").remove();
-                    //  flag = 0;
+
                     console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
 
                 }
@@ -51,8 +50,8 @@
 
             if (eventData.delegateTarget.value == "" || eventData.delegateTarget.value == null) {
                 if ($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount == 0) {
-                    $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s2' style='color:red'>Please Fill the First name</span>");
-                    //  flag1 = 1;
+                    $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s2' style='color:red'>Please Fill the Last name</span>");
+
                     console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
                 }
 
@@ -60,12 +59,94 @@
             } else {
                 {
                     $("#s2").remove();
-                    flag1 = 0;
+
                     console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
 
                 }
             }
         };
+
+        var validatePhoneNo = function(eventData) {
+            var pattern = /^\d{10}$/;
+            if (!pattern.test(eventData.delegateTarget.value)) {
+                if ($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount == 0) {
+                    $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s3' style='color:red'>*phone can only 10 digit number </span>");
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+                }
+
+
+            } else {
+                {
+                    $("#s3").remove();
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+
+                }
+            }
+        };
+
+        var validateOfficeNo = function(eventData) {
+            var pattern = /^\d{6,8}$/;
+            if (!pattern.test(eventData.delegateTarget.value)) {
+                if ($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount == 0) {
+                    $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s4' style='color:red'>*Office can min 6 digit & max 8 number </span>");
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+                }
+
+
+            } else {
+                {
+                    $("#s4").remove();
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+
+                }
+            }
+        };
+
+        var validateEmail = function(eventData) {
+            var pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/;
+            if (!pattern.test(eventData.delegateTarget.value)) {
+                if ($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount == 0) {
+                    $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s5' style='color:red'>*please fill valid email </span>");
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+                }
+
+
+            } else {
+                {
+                    $("#s5").remove();
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+
+                }
+            }
+        };
+
+        var validatePass = function(eventData) {
+            var pattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){6,16}/;
+            if (!pattern.test(eventData.delegateTarget.value)) {
+                if ($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount == 0) {
+                    $(eventData.delegateTarget.previousSibling.previousElementSibling).append(" <span id='s6' style='color:red'>*Use 8 characters with min 1 lower 1 upper case & digit! </span>");
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+                }
+
+
+            } else {
+                {
+                    $("#s6").remove();
+
+                    console.log($(eventData.delegateTarget.previousSibling.previousElementSibling).context.childElementCount);
+
+                }
+            }
+        };
+
+
 
 
         var handleBlurEvent = function(eventSource, eventData) {
@@ -75,6 +156,18 @@
                     break;
                 case "lastname":
                     validateLastName(eventData);
+                    break;
+                case "phone":
+                    validatePhoneNo(eventData);
+                    break;
+                case "office":
+                    validateOfficeNo(eventData);
+                    break;
+                case "email":
+                    validateEmail(eventData);
+                    break;
+                case "password":
+                    validatePass(eventData);
                     break;
                 default:
 
