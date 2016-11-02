b500797f715735e6b259de112180270e9a8a3ce9
rahulsapkal23 <rahul.sapkal23@neosofttech.com>
2016-11-02 17:11:48 +0530

    Assign 2B is modified but not commited so only for backup

--- static/js/Json.js
+++ static/js/Json.js
@@ -1,25 +1,11 @@
 $(document).ready(function () {
-  var a = {
-           Cars: [{
-               "CarType": "BMW",
-               "carID": "bmw123"
-           }, {
-               "CarType": "mercedes",
-               "carID": "merc123"
-           }, {
-               "CarType": "volvo",
-               "carID": "vol123r"
-           }, {
-               "CarType": "ford",
-               "carID": "ford123"
-           }]
-       };
-       $.each(a.Cars, function (key, value) {
-           $("#dropDownDest").append($('<option></option>').val(value.carID).html(value.CarType));
-       });
-
-       $('#dropDownDest').change(function () {
-           alert($(this).val());
-           //Code to select image based on selected car id
-       });
+  var ccc=[];
+  $.getJSON( "country.json", function(data) {
+    $.each(data,function (index,value) {
+      ccc[index]=value;
+    })
+  console.log("suceess");
+  console.log(ccc);
+  });
+       
   })
